c97d04d40e31d7b421eb4086dbe1fb37c6cf5345
ashok.yadav1209@gmail.com <87533791+ashok-cubic@users.noreply.github.com>
2023-12-27 18:55:42 +0530

    testCode

---
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/models/GooglePage.java
@@ -0,0 +1,52 @@
+package com.dsv.datafactory.file.extraction.processor.models;
+
+import java.util.ArrayList;
+
+@Data //we can use lombok builders to avoid setters and getters
+public class GooglePage {
+    private int width;
+    private int height;
+    private ArrayList<GoogleBlock> blocks;
+    private double confidence;
+    private TextProperty textProperty;
+
+    public void setConfidence(double confidence) {
+        this.confidence = confidence;
+    }
+
+    public void setBlocks(ArrayList<GoogleBlock> blocks) {
+        this.blocks = blocks;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setTextProperty(TextProperty textProperty) {
+        this.textProperty = textProperty;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public TextProperty getTextProperty() {
+        return textProperty;
+    }
+
+    public ArrayList<GoogleBlock> getBlocks() {
+        return blocks;
+    }
+}
---
+++ src/main/java/com/dsv/datafactory/file/extraction/processor/models/GoogleSymbol.java
@@ -0,0 +1,42 @@
+package com.dsv.datafactory.file.extraction.processor.models;
+
+@Data //we can use lombok builders to avoid setters and getters
+public class GoogleSymbol {
+    private TextProperty property;
+    private BoundingPoly boundingBox;
+    private String text;
+    private double confidence;
+
+    public void setProperty(TextProperty property) {
+        this.property = property;
+    }
+
+    public void setBoundingBox(BoundingPoly boundingBox) {
+        this.boundingBox = boundingBox;
+    }
+
+    public void setConfidence(double confidence) {
+        this.confidence = confidence;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public TextProperty getProperty() {
+        return property;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public BoundingPoly getBoundingBox() {
+        return boundingBox;
+    }
+}
+
